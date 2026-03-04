68d2c3b4012857c9606fdd3e1d81f947a265db26
sinaqvi008-sys <sinaqvi.008@gmail.com>
2026-03-05 03:12:21 +0530

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 
-export const SITE_NAME = 'Colony Shop';
+export const SITE_NAME = 'Tayat Studio';
 export const DEFAULT_CATEGORY_SLUG = 'ladies-suits';
 export const MAX_CART_ITEMS = 5;
 export const TIME_SLOTS = ['10:00-13:00', '13:00-16:00', '18:00-20:00'];
